6af39f562aa012dbcc5ac0ed2e34005801776dac
mleku <me@mleku.dev>
2025-07-09 12:49:19 +0100

    Enhance `HandleReq` in `socketapi`: implement filter completion logic, reverse-sort events by creation time, and log subscription details.

--- socketapi/handleReq.go
+++ socketapi/handleReq.go
@@ -1,7 +1,9 @@
 package socketapi
 
 import (
+	"fmt"
 	"orly.dev/chk"
+	"orly.dev/envelopes/closedenvelope"
 	"orly.dev/envelopes/eoseenvelope"
 	"orly.dev/envelopes/eventenvelope"
 	"orly.dev/envelopes/reqenvelope"
@@ -9,6 +11,7 @@ import (
 	"orly.dev/interfaces/server"
 	"orly.dev/log"
 	"orly.dev/publish"
+	"sort"
 )
 
 func (a *A) HandleReq(
@@ -27,15 +30,30 @@ func (a *A) HandleReq(
 	}
 	log.I.S(env)
 	var evs event.S
-	for _, f := range env.Filters.F {
+	// if the number of events on a filter matches the limit, mark the filter
+	// complete to prevent opening a subscription.
+	completed := make([]bool, len(env.Filters.F))
+	for i, f := range env.Filters.F {
 		var e event.S
 		if e, err = sto.QueryEvents(a.Context(), f); chk.E(err) {
 			// this one failed, maybe try another
 			err = nil
+			if f.Ids.Len() > 0 {
+				completed[i] = true
+			}
 			continue
 		}
 		evs = append(evs, e...)
+		log.I.S(f.Limit, len(evs), f.Ids.Len())
+		if (f.Limit != nil && int(*f.Limit) <= len(evs)) || f.Ids.Len() > 0 {
+			completed[i] = true
+		}
 	}
+	sort.Slice(
+		evs, func(i, j int) bool {
+			return evs[i].CreatedAt.I64() > evs[j].CreatedAt.I64()
+		},
+	)
 	for _, ev := range evs {
 		log.I.F("sending event\n%s", ev.Serialize())
 		var res *eventenvelope.Result
@@ -51,6 +69,30 @@ func (a *A) HandleReq(
 	if err = eoseenvelope.NewFrom(env.Subscription).Write(a.Listener); chk.E(err) {
 		return
 	}
+	// if all filters are complete, return instead of opening a subscription
+	complete := true
+	for _, c := range completed {
+		if !c {
+			complete = false
+			break
+		}
+	}
+	if complete {
+		log.I.F("all filters complete, returning")
+		if err = closedenvelope.NewFrom(
+			env.Subscription, []byte(fmt.Sprintf(
+				"subscription %s complete", env.Subscription.String(),
+			)),
+		).Write(a.Listener); chk.E(err) {
+			return
+		}
+		return
+	}
+	for _, f := range env.Filters.F {
+		log.I.F(
+			"opening subscription for %s %s", env.Subscription, f.Marshal(nil),
+		)
+	}
 	receiver := make(event.C, 32)
 	publish.P.Receive(
 		&W{
@@ -60,6 +102,5 @@ func (a *A) HandleReq(
 			Filters:  env.Filters,
 		},
 	)
-
 	return
 }
